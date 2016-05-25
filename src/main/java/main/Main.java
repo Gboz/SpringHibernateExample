a1e862a437b3a711c7bec6f077325eaa5ab3aa69
Grzegorz Bolesta <gbolesta21@gmail.com>
2016-05-25 15:03:24 +0200

    init

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import domain.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		Employee employee = new Employee();
+		employee.setName("Dzons");
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(employee);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+}
